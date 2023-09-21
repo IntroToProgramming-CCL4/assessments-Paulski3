9727c8cad69f22f247fda74917cb114cd0a6438d
Paulski3 <144930937+Paulski3@users.noreply.github.com>
2023-09-21 20:27:26 +0400

    Add files via upload

---
+++ Assessment 1/Chapter 1- Getting Started with Python/Exercises/Chapt 1 Ex 1 Print Strings.py
@@ -0,0 +1,3 @@
+print('Twinkle, twinkle, little star\n\tHow I wonder what you are')
+print('\t\tUp above the world so high\n\t\tLike a diamond in the sky')
+print('Twinkle, twinkle, little star\n\tHow I wonder what you are')
