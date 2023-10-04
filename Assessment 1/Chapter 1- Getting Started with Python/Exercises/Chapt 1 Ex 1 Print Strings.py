16a6b94ec38e1f211d66cc4cf628b7bf1930b04e
Paulski3 <144930937+Paulski3@users.noreply.github.com>
2023-10-04 21:17:50 +0400

    Update Chapt 1 Ex 1 Print Strings.py
    Added comments

--- Assessment 1/Chapter 1- Getting Started with Python/Exercises/Chapt 1 Ex 1 Print Strings.py
+++ Assessment 1/Chapter 1- Getting Started with Python/Exercises/Chapt 1 Ex 1 Print Strings.py
@@ -1,3 +1,3 @@
-print('Twinkle, twinkle, little star\n\tHow I wonder what you are')
-print('\t\tUp above the world so high\n\t\tLike a diamond in the sky')
-print('Twinkle, twinkle, little star\n\tHow I wonder what you are')
+print('Twinkle, twinkle, little star\n\tHow I wonder what you are') #Printing the string
+print('\t\tUp above the world so high\n\t\tLike a diamond in the sky') #Printing the string on the next line
+print('Twinkle, twinkle, little star\n\tHow I wonder what you are') #Printing another string
