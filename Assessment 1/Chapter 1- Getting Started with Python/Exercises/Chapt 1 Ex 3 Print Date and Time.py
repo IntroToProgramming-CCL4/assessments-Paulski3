b851e1ab6319711440f9719b7c1d929d1e883034
Paulski3 <144930937+Paulski3@users.noreply.github.com>
2023-10-19 12:44:10 +0400

    Add files via upload

---
+++ Assessment 1/Chapter 1- Getting Started with Python/Exercises/Chapt 1 Ex 3 Print Date and Time.py
@@ -0,0 +1,4 @@
+import datetime #importing datetime file into the code
+
+c_dt=datetime.datetime.now() #Assigning a variable
+print(c_dt.strftime("Current date: %d/%m/%Y\nCurrent time: %H:%M")) #Printing the current date and time
