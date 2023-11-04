d9a1c07f19ec71eb8ce37e1e97037e0e6ab4ee60
Paulski3 <144930937+Paulski3@users.noreply.github.com>
2023-11-05 03:00:13 +0400

    Add files via upload
    My code for:
    Exercise 1: Person ☑️
    Use a dictionary to store information about a person you know.Store their first name, last name, age, and the city in which they live. You
    should have keys such as first_name, last_name, age, and city. Print each piece of information stored in your dictionary.
    
    First Submission

---
+++ Assessment 1/Chapter 5- Dictionaries/Exercises/Chapt 5 Ex 1 Person.py
@@ -0,0 +1,6 @@
+# A dictionary of a person
+person = {'first_name': 'Paul', 'last_name': 'Sevillejo', 'age': 18, 'city': 'Dubai'}
+
+# Printing each information
+for key, value in person.items():
+    print(value)
