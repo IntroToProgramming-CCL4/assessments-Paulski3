75ef5de745101b8855ea052b3479c5036bb312de
Paulski3 <144930937+Paulski3@users.noreply.github.com>
2023-11-08 19:43:32 +0400

    Update Chapt 1 Ex 3 Print Date and Time.py
    My code for:
    Exercise 3: Print date and Time ☑️
    Write a Python program to display the current date and time.
    
    First Submission
    First edit: Added Comments
    Second edit: Changed the format

--- Assessment 1/Chapter 1- Getting Started with Python/Exercises/Chapt 1 Ex 3 Print Date and Time.py
+++ Assessment 1/Chapter 1- Getting Started with Python/Exercises/Chapt 1 Ex 3 Print Date and Time.py
@@ -1,4 +1,5 @@
-import datetime #importing datetime file into the code
+# Importing a constant
+import datetime
 
-c_dt=datetime.datetime.now() #Assigning a variable
-print(c_dt.strftime("Current date: %d/%m/%Y\nCurrent time: %H:%M")) #Printing the current date and time
+c_dt = datetime.datetime.now()    # Assigning a variable
+print(c_dt.strftime("\nCurrent date: %d/%m/%Y\nCurrent time: %H:%M"))   # Printing the current date and time
