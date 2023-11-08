455e36dd52ff0765a863d1e599d8a3a3a428b8ec
Paulski3 <144930937+Paulski3@users.noreply.github.com>
2023-11-08 19:08:36 +0400

    Update Chapt 1 Ex 1 Print Strings.py
    My code for:
    Exercise 1: Print Strings ☑️
    Write a Python program to print the following string in a specific format.
    
    Twinkle, twinkle, little star, How I wonder what you are! Up above the world so high, Like a diamond in the sky. Twinkle, twinkle, little star, How I wonder what you are
    
    First submission
    First edit: Added comments
    Second edit: Changed the whole program format

--- Assessment 1/Chapter 1- Getting Started with Python/Exercises/Chapt 1 Ex 1 Print Strings.py
+++ Assessment 1/Chapter 1- Getting Started with Python/Exercises/Chapt 1 Ex 1 Print Strings.py
@@ -1,3 +1,3 @@
-print('Twinkle, twinkle, little star\n\tHow I wonder what you are') #Printing the string
-print('\t\tUp above the world so high\n\t\tLike a diamond in the sky') #Printing the string on the next line
-print('Twinkle, twinkle, little star\n\tHow I wonder what you are') #Printing another string
+print('Twinkle, twinkle, little star\n\tHow I wonder what you are
+      \n\t\tUp above the world so high\n\t\tLike a diamond in the sky  
+      \nTwinkle, twinkle, little star\n\tHow I wonder what you are') 
