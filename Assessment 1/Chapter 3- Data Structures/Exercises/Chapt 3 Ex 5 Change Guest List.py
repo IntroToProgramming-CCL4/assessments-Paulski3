99e60d55ec70c593658e81315f52760aa07173cd
Paulski3 <144930937+Paulski3@users.noreply.github.com>
2023-11-08 21:11:04 +0400

    Update Chapt 3 Ex 5 Change Guest List.py
    Exercise 5: Change Guest List ☑️
    You just heard that one of your guests can’t make the dinner, so you need to send out a new set of invitations. You’ll have to think of someone else to invite.
    
    •Start with your program from Exercise 3-4. Add a print() call at the end of your program stating the name of the guest who can’t make it.
    •Modify your list, replacing the name of the guest who can’t make it with the name of the new person you are inviting.
    •Print a second set of invitation messages, one for each person who is still in your list.
    
    First Submission
    First edit: Deleted some unnecessary comments and added a new comment to show where the new program starts

--- Assessment 1/Chapter 3- Data Structures/Exercises/Chapt 3 Ex 5 Change Guest List.py
+++ Assessment 1/Chapter 3- Data Structures/Exercises/Chapt 3 Ex 5 Change Guest List.py
@@ -1,9 +1,11 @@
 # Chapt 3 Ex 4 program
-names = ['Phillip', 'EmJay', 'Julia', 'Conching', 'Juvy', 'Perlito']  # A list of names
-for name in names:
-    print("Join me to dinner", name)  # Invitation to dinner
+names = ['Phillip', 'EmJay', 'Julia', 'Conching', 'Juvy', 'Perlito']
 
-print("\n", names[1], "Cant make to dinner\n")  # Guest who cant make it
+for name in names:
+    print("Join me to dinner", name)
+
+# Chapt 3 Ex 5 program
+print("\n!" + names[1], "Cant make to dinner!\n")  # Guest who cant make it
 
 # Modifying the list
 remove = names.pop(1)  # Removing the guest that cant make it
