cb814c048cbf2b1e19e24f1c13ace29a6ecbbbaa
Paulski3 <144930937+Paulski3@users.noreply.github.com>
2023-11-08 21:21:43 +0400

    Update Chapt 4 Ex 1 Alien Colors #1.py
    My Code for: 
    Exercise 1: Alien Colors #1 ☑️
    Imagine an alien was just shot down in a game. Create a variable called alien_color and assign it a value of 'green', 'yellow', or 'red'.
    •Write an if statement to test whether the alien’s color is green. If it is, print a message that the player just earned 5 points.
    •Write one version of this program that passes the if test and another that fails. (The version that fails will have no output.)
    
    First Submission
    First edit: Cleaned the program and added new comments for better comprehension

--- Assessment 1/Chapter 4- Control Flow/Exercises/Chapt 4 Ex 1 Alien Colors #1.py
+++ Assessment 1/Chapter 4- Control Flow/Exercises/Chapt 4 Ex 1 Alien Colors #1.py
@@ -1,11 +1,15 @@
 # First Version
-alien_color = 'green'
+alien_color = 'green'   # Create a variable called alien_color and assign it a value of 'green'
 
+''' Write an if statement to test whether the alien’s color is green. 
+If it is, print a message that the player just earned 5 points. '''
 if alien_color == 'green':
-    print('You have earned 5 points')
+    print('\nYou have earned 5 points')
 
 # Second Version
+'''Write one version of this program that passes the 
+if test and another that fails. (The version that fails will have no output.)'''
 alien_color = 'red'
 
 if alien_color == 'green':
-    print('You have earned 5 points')
+    print('\nYou have earned 5 points')
