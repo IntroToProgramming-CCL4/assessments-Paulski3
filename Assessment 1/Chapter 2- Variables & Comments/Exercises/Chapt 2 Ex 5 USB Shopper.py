af39ffac440190338bbb07b75486bf7ae939acbc
Paulski3 <144930937+Paulski3@users.noreply.github.com>
2023-11-08 20:49:43 +0400

    Update Chapt 2 Ex 5 USB Shopper.py
    My code for:
    Exercise 5: USB Shopper ☑️
    A girl heads to a computer shop to buy some USB sticks. She loves USB sticks and wants as many as she can get for £50. They are £6 each.
    
    Write a programme that calculates how many USB sticks she can buy and how many pounds she will have left.
    
    You will to use the arithmetic operators to complete this exercise.
    
    First Submission
    First edit: Cleaned up my program

--- Assessment 1/Chapter 2- Variables & Comments/Exercises/Chapt 2 Ex 5 USB Shopper.py
+++ Assessment 1/Chapter 2- Variables & Comments/Exercises/Chapt 2 Ex 5 USB Shopper.py
@@ -1,10 +1,11 @@
-budget = 50  # budget given
-price = 6  # 1 USB price
+# Variables
+budget = 50  # Budget given
+price = 6  # Price of 1 USB
 
 # Calculator
 USB_bought = (budget // price)  # Calculates possible quantity of USB with given budget
 left = budget - (USB_bought * price)  # Calculates the remaining money
 
 # Results
-print("You can buy", USB_bought, "USB sticks for", budget, "pounds")
+print("\nYou can buy", USB_bought, "USB sticks for", budget, "pounds")
 print("Change:", left, "pounds")
