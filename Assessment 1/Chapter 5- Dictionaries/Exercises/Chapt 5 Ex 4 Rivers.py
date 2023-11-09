002fe55f55eb936b1d343d2c671554a533aa374f
Paulski3 <144930937+Paulski3@users.noreply.github.com>
2023-11-09 13:30:33 +0400

    Update Chapt 5 Ex 4 Rivers.py
    My code for:
    Exercise 4: Rivers ☑️
    Make a dictionary containing three major rivers and the country each river runs through. One key-value pair might be 'nile' :  'egypt'.
    Use a loop to print a sentence about each river, such as The Nile runs through Egypt.
    Use a loop to print the name of each river included in the dictionary.
    Use a loop to print the name of each country included in the dictionary.
    
    First Submission
    First edit: Added comments

--- Assessment 1/Chapter 5- Dictionaries/Exercises/Chapt 5 Ex 4 Rivers.py
+++ Assessment 1/Chapter 5- Dictionaries/Exercises/Chapt 5 Ex 4 Rivers.py
@@ -1,13 +1,16 @@
-#
+# Dictionary of rivers and its countries
 rivers = {'nile': 'egypt', 'yangtze': 'china', 'missouri': 'united state'}
 
+# Loop of rivers information
 for key, value in rivers.items():
     print('The', key.title(), 'runs through', value.title())
 
+# Loop to print the name of each river included in the dictionary.
 print('\nRivers:')
 for key in rivers.keys():
     print('-' + key.title())
 
+# Loop to print the name of each country included in the dictionary.
 print('\nCountries:')
 for value in rivers.values():
     print('-' + value.title())
